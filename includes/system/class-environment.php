ac036d04916546ec79fb6f82c8d88d487d1842ad
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-05-01 18:00:56 +0200

    Fixes issues with IIS/Win10.

--- includes/system/class-environment.php
+++ includes/system/class-environment.php
@@ -35,6 +35,7 @@ class Environment {
 	 */
 	public static function init() {
 		$plugin_path         = str_replace( APCM_SLUG . '/includes/system/', APCM_SLUG . '/', plugin_dir_path( __FILE__ ) );
+		$plugin_path         = str_replace( APCM_SLUG . '\includes\system/', APCM_SLUG . '/', $plugin_path );
 		$plugin_url          = str_replace( APCM_SLUG . '/includes/system/', APCM_SLUG . '/', plugin_dir_url( __FILE__ ) );
 		$plugin_relative_url = str_replace( get_site_url() . '/', '', $plugin_url );
 		define( 'APCM_PLUGIN_DIR', $plugin_path );
