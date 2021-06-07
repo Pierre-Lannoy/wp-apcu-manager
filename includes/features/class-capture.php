c66cc645b8753fe152e0dc30c3d645deab216df2
Pierre Lannoy <pierre@lannoy.fr>
2021-06-07 09:39:54 +0200

    Verify prerequisites before trying to collate metrics.

--- includes/features/class-capture.php
+++ includes/features/class-capture.php
@@ -102,96 +102,100 @@ class Capture {
 	 * @since    2.4.0
 	 */
 	public static function metrics() {
-		$span     = \DecaLog\Engine::tracesLogger( APCM_SLUG )->start_span( 'Metrics collation' );
-		$cache_id = 'metrics/lastcheck';
-		$metrics  = Cache::get_global( $cache_id );
-		if ( ! isset( $metrics ) ) {
-			try {
-				$memory  = apcu_sma_info( false );
-				$value   = apcu_cache_info( true );
-				$metrics = [];
-				if ( array_key_exists( 'num_seg', $memory ) && array_key_exists( 'seg_size', $memory ) && array_key_exists( 'avail_mem', $memory ) ) {
-					$mem_total = (int) ( $memory['num_seg'] * $memory['seg_size'] );
-					$mem_used  = (int) ( $mem_total - $memory['avail_mem'] );
-					if ( 0 < $mem_total ) {
-						$metrics['mem'] = $mem_used / $mem_total;
-					} else {
-						$metrics['mem'] = 0.0;
+		if ( function_exists( 'apcu_cache_info' ) && function_exists( 'apcu_sma_info' ) ) {
+			$span     = \DecaLog\Engine::tracesLogger( APCM_SLUG )->start_span( 'Metrics collation' );
+			$cache_id = 'metrics/lastcheck';
+			$metrics  = Cache::get_global( $cache_id );
+			if ( ! isset( $metrics ) ) {
+				try {
+					$memory  = apcu_sma_info( false );
+					$value   = apcu_cache_info( true );
+					$metrics = [];
+					if ( array_key_exists( 'num_seg', $memory ) && array_key_exists( 'seg_size', $memory ) && array_key_exists( 'avail_mem', $memory ) ) {
+						$mem_total = (int) ( $memory['num_seg'] * $memory['seg_size'] );
+						$mem_used  = (int) ( $mem_total - $memory['avail_mem'] );
+						if ( 0 < $mem_total ) {
+							$metrics['mem'] = $mem_used / $mem_total;
+						} else {
+							$metrics['mem'] = 0.0;
+						}
 					}
-				}
-				if ( array_key_exists( 'num_slots', $value ) && array_key_exists( 'num_entries', $value ) ) {
-					$total = (int) $value['num_slots'];
-					$used  = (int) $value['num_entries'];
-					if ( 0 < $total ) {
-						$metrics['slot'] = $used / $total;
-					} else {
-						$metrics['slot'] = 0.0;
+					if ( array_key_exists( 'num_slots', $value ) && array_key_exists( 'num_entries', $value ) ) {
+						$total = (int) $value['num_slots'];
+						$used  = (int) $value['num_entries'];
+						if ( 0 < $total ) {
+							$metrics['slot'] = $used / $total;
+						} else {
+							$metrics['slot'] = 0.0;
+						}
 					}
-				}
-				if ( array_key_exists( 'num_hits', $value ) && array_key_exists( 'num_misses', $value ) && array_key_exists( 'num_inserts', $value ) ) {
-					$hit  = (int) $value['num_hits'];
-					$miss = (int) $value['num_misses'];
-					$ins  = (int) $value['num_inserts'];
-					if ( 0 < $hit + $miss ) {
-						$metrics['hit_ratio'] = $hit / ( $hit + $miss );
-					} else {
-						$metrics['hit_ratio'] = 0.0;
+					if ( array_key_exists( 'num_hits', $value ) && array_key_exists( 'num_misses', $value ) && array_key_exists( 'num_inserts', $value ) ) {
+						$hit  = (int) $value['num_hits'];
+						$miss = (int) $value['num_misses'];
+						$ins  = (int) $value['num_inserts'];
+						if ( 0 < $hit + $miss ) {
+							$metrics['hit_ratio'] = $hit / ( $hit + $miss );
+						} else {
+							$metrics['hit_ratio'] = 0.0;
+						}
+						if ( 0 < $hit + $miss + $ins ) {
+							$metrics['ins_ratio'] = $ins / ( $hit + $miss + $ins );
+						} else {
+							$metrics['ins_ratio'] = 0.0;
+						}
 					}
-					if ( 0 < $hit + $miss + $ins ) {
-						$metrics['ins_ratio'] = $ins / ( $hit + $miss + $ins );
-					} else {
-						$metrics['ins_ratio'] = 0.0;
-					}
-				}
-				if ( array_key_exists( 'block_lists', $memory ) && is_array( $memory['block_lists'] ) ) {
-					$frag_small = 0;
-					$frag_big   = 0;
-					foreach ( $memory['block_lists'] as $chunk ) {
-						if ( is_array( $chunk ) ) {
-							foreach ( $chunk as $block ) {
-								if ( array_key_exists( 'size', $block ) ) {
-									/* Like apc.php, only consider blocks <5M for the fragmentation % */
-									if ( $block['size'] < ( 5 * 1024 * 1024 ) ) {
-										$frag_small = ( (int) $block['size'] ) + $frag_small;
-									} else {
-										$frag_big = ( (int) $block['size'] ) + $frag_big;
+					if ( array_key_exists( 'block_lists', $memory ) && is_array( $memory['block_lists'] ) ) {
+						$frag_small = 0;
+						$frag_big   = 0;
+						foreach ( $memory['block_lists'] as $chunk ) {
+							if ( is_array( $chunk ) ) {
+								foreach ( $chunk as $block ) {
+									if ( array_key_exists( 'size', $block ) ) {
+										/* Like apc.php, only consider blocks <5M for the fragmentation % */
+										if ( $block['size'] < ( 5 * 1024 * 1024 ) ) {
+											$frag_small = ( (int) $block['size'] ) + $frag_small;
+										} else {
+											$frag_big = ( (int) $block['size'] ) + $frag_big;
+										}
 									}
 								}
 							}
 						}
+						if ( 0 < $frag_small + $frag_big ) {
+							$metrics['frag'] = $frag_small / ( $frag_small + $frag_big );
+						} else {
+							$metrics['frag'] = 0.0;
+						}
 					}
-					if ( 0 < $frag_small + $frag_big ) {
-						$metrics['frag'] = $frag_small / ( $frag_small + $frag_big );
-					} else {
-						$metrics['frag'] = 0.0;
-					}
+					Cache::set_global( $cache_id, $metrics, 'metrics' );
+					\DecaLog\Engine::eventsLogger( APCM_SLUG )->debug( 'APCu is enabled. Statistics recorded.' );
+				} catch ( \Throwable $e ) {
+					$metrics = null;
+					\DecaLog\Engine::eventsLogger( APCM_SLUG )->error( sprintf( 'Unable to query APCu status: %s.', $e->getMessage() ), [ 'code' => $e->getCode() ] );
 				}
-				Cache::set_global( $cache_id, $metrics, 'metrics' );
-				\DecaLog\Engine::eventsLogger( APCM_SLUG )->debug( 'APCu is enabled. Statistics recorded.' );
-			} catch ( \Throwable $e ) {
-				$metrics = null;
-				\DecaLog\Engine::eventsLogger( APCM_SLUG )->error( sprintf( 'Unable to query APCu status: %s.', $e->getMessage() ), [ 'code' => $e->getCode() ] );
 			}
+			if ( isset( $metrics ) ) {
+				$monitor = \DecaLog\Engine::metricsLogger( APCM_SLUG );
+				if ( array_key_exists( 'mem', $metrics ) ) {
+					$monitor->createProdGauge( 'memory_saturation', $metrics['mem'], 'APCu used memory - [percent]' );
+				}
+				if ( array_key_exists( 'frag', $metrics ) ) {
+					$monitor->createProdGauge( 'memory_fragmentation', $metrics['frag'], 'APCu fragmented memory - [percent]' );
+				}
+				if ( array_key_exists( 'slot', $metrics ) ) {
+					$monitor->createProdGauge( 'key_saturation', $metrics['slot'], 'APCu used keys - [percent]' );
+				}
+				if ( array_key_exists( 'hit_ratio', $metrics ) ) {
+					$monitor->createProdGauge( 'hit_ratio', $metrics['hit_ratio'], 'APCu hit ratio - [percent]' );
+				}
+				if ( array_key_exists( 'ins_ratio', $metrics ) ) {
+					$monitor->createProdGauge( 'insert_ratio', $metrics['ins_ratio'], 'APCu insert ratio - [percent]' );
+				}
+			}
+			\DecaLog\Engine::tracesLogger( APCM_SLUG )->end_span( $span );
+		} else {
+			\DecaLog\Engine::eventsLogger( APCM_SLUG )->debug( 'APCu is disabled. No metrics to publish.' );
 		}
-		if ( isset( $metrics ) ) {
-			$monitor = \DecaLog\Engine::metricsLogger( APCM_SLUG );
-			if ( array_key_exists( 'mem', $metrics ) ) {
-				$monitor->createProdGauge( 'memory_saturation', $metrics['mem'], 'APCu used memory - [percent]' );
-			}
-			if ( array_key_exists( 'frag', $metrics ) ) {
-				$monitor->createProdGauge( 'memory_fragmentation', $metrics['frag'], 'APCu fragmented memory - [percent]' );
-			}
-			if ( array_key_exists( 'slot', $metrics ) ) {
-				$monitor->createProdGauge( 'key_saturation', $metrics['slot'], 'APCu used keys - [percent]' );
-			}
-			if ( array_key_exists( 'hit_ratio', $metrics ) ) {
-				$monitor->createProdGauge( 'hit_ratio', $metrics['hit_ratio'], 'APCu hit ratio - [percent]' );
-			}
-			if ( array_key_exists( 'ins_ratio', $metrics ) ) {
-				$monitor->createProdGauge( 'insert_ratio', $metrics['ins_ratio'], 'APCu insert ratio - [percent]' );
-			}
-		}
-		\DecaLog\Engine::tracesLogger( APCM_SLUG )->end_span( $span );
 	}
 
 	/**
