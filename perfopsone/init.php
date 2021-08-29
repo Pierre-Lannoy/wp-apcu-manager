a0dccbde16d2414bcf20f8dc2daee7b42328fa0e
Pierre Lannoy <pierre@lannoy.fr>
2021-08-29 19:14:00 +0200

    Modify init sequence to allow unconditional assets enqueuing.

--- perfopsone/init.php
+++ perfopsone/init.php
@@ -15,10 +15,7 @@ if ( ! defined( 'PERFOO_INITIALIZED' ) ) {
 	define( 'PERFOO_PRODUCT_URL', 'https://perfops.one' );
 	define( 'PERFOO_PRODUCT_SHORTNAME', 'PerfOps One' );
 	define( 'PERFOO_INITIALIZED', true );
+	define( 'PERFOO_ASSETS_ID', 'poo-assets' );
+	define( 'PERFOO_ASSETS_CSS', plugins_url( 'perfopsone/assets/perfopsone.min.css', plugin_basename( dirname( __FILE__ ) ) ) );
 }
-
-//TODO: remove debug require_once statements
-
-require_once __DIR__ . '/functions.php';
-require_once __DIR__ . '/autoload.php';
 
