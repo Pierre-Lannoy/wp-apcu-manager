0c020d9130c16f0699fdc565ff8a38cb8e2732dc
Pierre Lannoy <pierre@lannoy.fr>
2021-09-06 21:46:15 +0200

    Finally fixes the locale in which the plugin is shown.

--- includes/plugin/class-initializer.php
+++ includes/plugin/class-initializer.php
@@ -37,10 +37,10 @@ class Initializer {
 	public function initialize() {
 		\APCuManager\System\Sitehealth::init();
 		\APCuManager\System\APCu::init();
-		if ( 'en_US' !== determine_locale() ) {
+		//if ( 'en_US' !== determine_locale() ) {
 			unload_textdomain( APCM_SLUG );
 			load_plugin_textdomain( APCM_SLUG );
-		}
+		//}
 	}
 
 	/**
