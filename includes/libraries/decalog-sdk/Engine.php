bc893b165c2396105a6ea55015d60f85792dbd0e
Pierre Lannoy <pierre@hosterra.eu>
2024-05-28 13:58:35 +0200

    Ready to release 4.0.0 on wp.org

--- includes/libraries/decalog-sdk/Engine.php
+++ includes/libraries/decalog-sdk/Engine.php
@@ -26,7 +26,7 @@ class Engine {
 	 * @since  1.0.0
 	 * @var    string    $version    Maintains the engine version.
 	 */
-	private static $version = '4.2.0';
+	private static $version = '5.0.0';
 
 	/**
 	 * The logger instances and parameters.
